7e697ec3d949cd88ced21b053f91c9e2c6ae7838
NoNormalCreeper <2560359315@qq.com>
2025-04-23 13:53:35 +0800

    feat: Enhance visualization functions for game board and actions, and add DBT feature visualization

--- train/visualize.py
+++ train/visualize.py
@@ -1,4 +1,6 @@
 from constants import *
+from models import Board, Game, FeatureExtractor
+from dbt import DbtFeatureExtractor
 
 
 def cyan(text: str) -> str:
@@ -56,6 +58,84 @@ def visualize_blocks(blocks: list[Block]) -> None:
         visualize_block(block)
         print("-" * 20)
         print("\n")
+        
+
+def visualize_board(board: Board) -> None:
+    """
+    可视化棋盘
+    
+    :param board: 棋盘对象
+    :return: None
+    """
+    # 棋盘边界
+    horizontal_border = cyan("+" + "-" * (board.size.width * 2 - 1) + "+")
+    
+    # 显示棋盘（从上到下）
+    print(horizontal_border)
+    for y in range(board.size.height - 1, -1, -1):  # 从上到下显示
+        row = []
+        for x in range(board.size.width):
+            cell = board.squares[y][x]
+            if cell is None:
+                row.append(" ")  # 空格子
+            else:
+                row.append(cell.label)  # 使用方块的标签
+        print(cyan("| ") + " ".join(row) + cyan(" |"))
+    print(horizontal_border)
+    
+    # 显示底部坐标
+    print("  " + " ".join(str(x) for x in range(board.size.width)))
+
+def visualize_action(game: Game, action: BlockStatus) -> None:
+    """
+    可视化游戏动作
+    
+    :param game: 游戏对象
+    :param action: 动作对象
+    :return: None
+    """
+    print(f"当前分数: {game.score}")
+    print(f"当前方块: {game.upcoming_blocks[0].label}")
+    print(f"最佳操作: degree={action.rotation.label}, x={action.x_offset}, score={action.assessment_score}")
+    visualize_occupied(action.rotation.occupied, action.rotation.size)
+
+def visualize_game(game: Game, action: BlockStatus) -> None:
+    """
+    可视化游戏操作
+    
+    :param game: 游戏对象
+    :param action: 动作对象
+    :return: None
+    """
+    print("当前操作:")
+    visualize_action(game, action)
+    print("棋盘状态:")
+    visualize_board(game.board)
+    print("即将出现的方块:")
+    for block in game.upcoming_blocks:
+        print(f"  {block.label}")
+    print("-" * 20)
+
+def visualize_dbt_feature(features: list[int]):
+    """
+    可视化 DBT 特征
+    
+    :param feature: DBT 特征对象
+    :return: None
+    """
+    print("DBT 特征:")
+    feature_texts = [
+        f"着陆高度: {features[0]}",
+        f"被侵蚀的方块数量: {features[1]}",
+        f"行转换方块数: {features[2]}",
+        f"列转换方块数: {features[3]}",
+        f"空穴数量: {features[4]}",
+        f"棋盘井特征量: {features[5]}",
+        f"空穴深度: {features[6]}",
+        f"有空穴的行数: {features[7]}"
+    ]
+    print("  " + " | ".join(feature_texts))
+    
 
 
 if __name__ == "__main__":
